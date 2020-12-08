acf307c1b3d730c16248a84408de8aaf676318d1
Reina Tochigi <tocchy@yutakanoMacBook-Pro.local>
2020-12-08 22:51:43 +0900

    assetsPublicPathの設定変更

--- config/index.js
+++ config/index.js
@@ -94,7 +94,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     proxyTable: {
       '/todo': {
